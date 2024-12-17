2363830281b55941de7cb07a671bb2589ebbf748
flx-lenovo-ubuntu <felixporzig@gmail.com>
2024-12-17 21:51:03 +0100

    modified:   get_next_line.c modified:   get_next_line.h modified:   get_next_line_utils.c

--- get_next_line.h
+++ get_next_line.h
@@ -1,7 +1,19 @@
-#ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fporzig <fporzig@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/17 21:48:37 by fporzig           #+#    #+#             */
+/*   Updated: 2024/12/17 21:50:01 by fporzig          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include <stddef.h>
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stddef.h>
 
 char	*get_next_line(int fd);
 char	*ft_strchr(const char *s, int c);
