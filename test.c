af45932c83902ea9e36a5ccaf58cebe7492db041
flx-42-school <fporzig@student.42berlin.de>
2025-01-13 15:22:09 +0100

    new file:   gnl-station-tester new file:   gnlTester modified:   test.c

--- test.c
+++ test.c
@@ -16,24 +16,32 @@
 #include <stdlib.h>
 #include "get_next_line.h"
 
-int	main(void)
+int main(int argc, char **argv)
 {
-	int		fd;
-	char	*line;
+    int     fd;
+    char    *line;
 
-	fd = open("file.txt", O_RDONLY);
-	if (fd < 0)
-	{
-		perror("Error opening file");
-		return (1);
-	}
-	line = get_next_line(fd);
-	while (line)
-	{
-		printf("%s", line);
-		free(line);
-		line = get_next_line(fd);
-	}
-	close(fd);
-	return (0);
+    if (argc < 2)
+    {
+        fprintf(stderr, "Usage: %s <file1> <file2> ... <fileN>\n", argv[0]);
+        return (1);
+    }
+    for (int i = 1; i < argc; i++)
+    {
+        fd = open(argv[i], O_RDONLY);
+        if (fd < 0)
+        {
+            perror("Error opening file");
+            continue;
+        }
+        line = get_next_line(fd);
+        while (line)
+        {
+            printf("%s", line);
+            free(line);
+            line = get_next_line(fd);
+        }
+        close(fd);
+    }
+    return (0);
 }
